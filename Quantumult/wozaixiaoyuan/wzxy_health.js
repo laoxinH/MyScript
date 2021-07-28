25faf4ad8e685eb92bdf2ff8e0bb6028fece91dd
laoxinH <53307507+laoxinH@users.noreply.github.com>
2021-07-28 21:54:55 +0800

    Update wzxy_health.js

--- Quantumult/wozaixiaoyuan/wzxy_health.js
+++ Quantumult/wozaixiaoyuan/wzxy_health.js
@@ -43,15 +43,16 @@ if (!cookieKey){
     $laoxin.msg($laoxin.name,`当前cookie:${cookieVal}---已失效`,"请打开我在校园小程序--\"我的\"重新获取!");
     $laoxin.done();
 } else {
-    start().then();
+    start().then((x)=>{return x});
 }
 async function start() {
     if (!data_answers.areacode){
-        getAreCode(); 
+        getAreCode();
     }
     getRegNum();
     await $laoxin.wait(1000);
     register();
+    return;
 }
 //打卡方法
 function register() {
