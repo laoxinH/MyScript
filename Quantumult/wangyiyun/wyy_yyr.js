00de7de5cd9f93cc30f42419db9cf3177f9dd15c
laoxinH <53307507+laoxinH@users.noreply.github.com>
2021-10-20 10:10:09 +0800

    添加账号密码登录，添加回复评论任务

--- Quantumult/wangyiyun/wyy_yyr.js
+++ Quantumult/wangyiyun/wyy_yyr.js
@@ -735,7 +735,6 @@ function getOpts(data,url) {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36"
         }
     };
-    console.log(opts)
     return opts;
 }
 
