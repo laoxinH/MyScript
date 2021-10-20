55609a7b9a90827dbd80c7ff367eefd00f06e97c
laoxinH <53307507+laoxinH@users.noreply.github.com>
2021-10-20 10:05:38 +0800

    更新说明

--- laoxinH_config.js
+++ laoxinH_config.js
@@ -1,7 +1,7 @@
 module.exports = {
     wangyi : {
         // 说明: 若填写cookie将直接通过cookie登录,电话号码登录将会失效(优先级:cookie > 电话登录)
-        // 因此cookie 和 电话二选一即可
+        // 因此cookie 和 电话二选一即可(推荐cookie登录)
         cookie : "",  // 网易云cookie填写处(注意不要有cookie结尾不能有换行获回车符号"\n"等)
         // 电话登录配置
         phone: "",    // 电话号码
